50d67d0079f94df960e2105d5cddb453e4177713
KitaPDev <kita.pairojtanachai@gmail.com>
2018-11-22 11:58:55 +0700

    kita 22/11/2018 11:58

--- Library/PettyCash_LIB_ITF/src/com/kita/pettycash/client/interfaces/AsyncResponse.java
+++ Library/PettyCash_LIB_ITF/src/com/kita/pettycash/client/interfaces/AsyncResponse.java
@@ -1,6 +1,12 @@
 package com.kita.pettycash.client.interfaces;
 
 
+import com.kitap.lib.bean.BEANPettyCashTransaction;
+
+import java.util.List;
+
 public interface AsyncResponse {
+
     void processFinish(Object output) throws Exception;
+    void processFinish(List<BEANPettyCashTransaction> lsBEANPettyCashTransaction) throws Exception;
 }
